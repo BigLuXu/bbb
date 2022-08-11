67da5068814252020c9d35a8b5e04120cb9a3e5c
BigLuXu <xulu@karboncard.com>
2022-08-11 23:27:41 +0800

    update webpack

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
+import './App.css'
+import { f1 } from './filter.js'
 function xx() {
 	return () => 3
 }
-
+f1()
+console.log(xx()());
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,32 @@
 const path = require('path')
 module.exports = {
-	entry: './src/index.js',
+	entry: 'src/index.tsx',
 	output: {
 		// 文件的输出路径
 		path: path.resolve(__dirname, 'dist'),
 		// 文件名
-		filename: 'main3.js',
+		filename: 'static/js/[name].js',
 	},
 	// 加载器
 	module: {
 		rules: [
-			// loader配置
+			{
+				test: /\.css$/,
+				use: [
+					// [style-loader](/loaders/style-loader)
+					{ loader: 'style-loader' },
+					// [css-loader](/loaders/css-loader)
+					{
+						loader: 'css-loader',
+						options: {
+							modules: true,
+						},
+					},
+					// [sass-loader](/loaders/sass-loader)
+					{ loader: 'postcss-loader' },
+				],
+			},
+			{ test: /\.ts$/, use: 'ts-loader' },
 		],
 	},
 	plugins: [
